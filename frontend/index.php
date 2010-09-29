376337969c1875dbff7dff56a8f9cd59fb643b4d
Jehan-Guillaume (ioguix) de Rorthais <jgdr@dalibo.com>
2010-09-29 14:40:38 +0200

    move queries from the main index.php loop to an outside loop one-in-all query

--- frontend/index.php
+++ frontend/index.php
@@ -3,11 +3,19 @@ $title = 'Known hosts';
 
 require('./intro.php');
 
-$query = "SELECT hostname, count(distinct service) AS services
-FROM services
-WHERE hostname NOT LIKE 'gateway-%'
-GROUP BY hostname
-ORDER BY hostname";
+$query = "SELECT t.hostname, t.pg_serv, t.sys_serv, t.srv_serv, t.all, (t.load).timet AS last_time_load, (t.load).value AS load
+FROM (
+	SELECT hostname,
+		sum((substr(service, 1, 5) = 'PGSQL')::int) AS pg_serv,
+		sum((substr(service, 1, 5) = 'SYSTM')::int) AS sys_serv,
+		sum((substr(service, 1, 5) = 'SERVC')::int) as srv_serv,
+		count(service) AS all,
+		get_last_value(hostname, 'SYSTM - Load', 'load1') AS load
+	FROM (SELECT distinct hostname, service FROM services) t
+	WHERE hostname NOT LIKE 'gateway-%'
+	GROUP BY hostname
+	ORDER BY 1,2
+) AS t";
 
 $res = pg_query($dbh, $query);
 
@@ -21,47 +29,19 @@ require('./menu.php');
 printf("<h1>%s</h1>", htmlentities($title));
 
 while ($host = pg_fetch_array($res)) {
-    // get last available load
-    $loadquery = "SELECT to_char(timet, 'YYYY-mm-DD HH:MM:SS') AS lasttime, value FROM get_last_value('".pg_escape_string($host['hostname'])."','SYSTM - Load','load1') AS (timet timestamptz, value numeric)";
-    $loadres = pg_query($dbh, $loadquery);
-    if (pg_num_rows($loadres) > 0)
-        $load = pg_fetch_array($loadres);
-    else
-        $load = false;
-    // get number of services
-    $squery = "SELECT substr(service, 1, 5) AS service, count(*) AS total
-      FROM (SELECT distinct service FROM services
-            WHERE hostname='".pg_escape_string($host['hostname'])."'
-              AND substr(service, 1, 5) in ('PGSQL', 'SERVC', 'SYSTM')) AS t
-      GROUP BY 1";
-    $sres = pg_query($dbh, $squery);
-    $servcservices = 0;
-    $systmservices = 0;
-    $pgsqlservices = 0;
-    while ($line = pg_fetch_array($sres))
-    {
-
-        if ($line['service'] == 'PGSQL')
-            $pgsqlservices = $line['total'];
-        else if ($line['service'] == 'SERVC')
-            $servcservices = $line['total'];
-        else if ($line['service'] == 'SYSTM')
-            $systmservices = $line['total'];
-    }
 
 	printf("<div><h2><a href=\"services.php?hostname=%s\">%s</a></h2>\n",
 		htmlentities($host['hostname']), htmlentities($host['hostname'])
     );
 
 	printf("<p>%u devices monitored - %u system, %u PostgreSQL, %u others</p>\n",
-		htmlentities($host['services']),
-		htmlentities($systmservices), htmlentities($pgsqlservices), htmlentities($servcservices)
+		$host['all'], $host['sys_serv'], $host['pg_serv'], $host['srv_serv']
     );
 
-    if ($load && isset($load['value']))
-	    printf("<p>Load: %s (last update %s).</p>\n",
-	    	htmlentities($load['value']), htmlentities($load['lasttime'])
-	);
+    if (!empty($host['load']))
+		printf("<p>Load: %s (last update %s).</p>\n",
+			htmlentities($host['load']), htmlentities($host['last_time_load'])
+		);
 }
 
 require('./outro.php');
