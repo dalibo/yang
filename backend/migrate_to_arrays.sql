a4dd624b924e86602f28320a95518c6c378fb193
Marc Cousin <marc.cousin@dalibo.com>
2012-12-10 14:12:50 +0100

    Add the migrate_to_arrays.sql script. This performs the schema change. All PL functions should then be reloaded

---
+++ backend/migrate_to_arrays.sql
@@ -0,0 +1,7 @@
+\t on
+\a
+\o /tmp/script
+WITH t1 as (SELECT relname as tbl, relname || '_tmp' as tbltmp from pg_class where relname ~ '^counters_detail_') 
+select 'CREATE TABLE ' || tbltmp || '(records counters_detail[]); INSERT INTO ' || tbltmp || ' select array_agg(row(timet,value)::counters_detail) from '|| tbl || ' group by date_trunc(''day'',timet); DROP TABLE ' || tbl ||'; ALTER TABLE ' ||  tbltmp || ' RENAME TO '|| tbl || ';' from t1;
+\o
+\i /tmp/script
