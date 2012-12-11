c20d30c7d079a7a32d4c4e88c6625d7cee73610c
Nicolas Thauvin <nicolas.thauvin@dalibo.com>
2012-12-11 10:40:24 +0100

    fix migrate_to_arrays.sql after adding the date columns for records in counter_details tables

--- backend/migrate_to_arrays.sql
+++ backend/migrate_to_arrays.sql
@@ -1,7 +1,7 @@
 \t on
 \a
 \o /tmp/script
-WITH t1 as (SELECT relname as tbl, relname || '_tmp' as tbltmp from pg_class where relname ~ '^counters_detail_') 
-select 'CREATE TABLE ' || tbltmp || '(records counters_detail[]); INSERT INTO ' || tbltmp || ' date_trunc(''day'',timet),select array_agg(row(timet,value)::counters_detail) from '|| tbl || ' group by date_trunc(''day'',timet); DROP TABLE ' || tbl ||'; ALTER TABLE ' ||  tbltmp || ' RENAME TO '|| tbl || ';' from t1;
+WITH t1 as (SELECT relname as tbl, relname || '_tmp' as tbltmp from pg_class where relname ~ '^counters_detail_' and relkind = 'r') 
+select 'CREATE TABLE ' || tbltmp || '(date_records date, records counters_detail[]); INSERT INTO ' || tbltmp || ' SELECT date_trunc(''day'',timet),array_agg(row(timet,value)::counters_detail) FROM '|| tbl || ' GROUP BY date_trunc(''day'',timet); DROP TABLE ' || tbl ||'; ALTER TABLE ' ||  tbltmp || ' RENAME TO '|| tbl || ';' from t1;
 \o
 \i /tmp/script
